d05a396778fb6995bff9405eaf2fc5ed47d96248
Steven <syhoung1@gmail.com>
2017-06-28 15:50:48 +0000

    routing fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,7 @@
 Rails.application.routes.draw do
-  get 'home_listings/index'
-
-  get 'home_listings/show'
-
-  get 'home_listings/edit'
-
-  get 'welcome/index'
-
-  get 'welcome/about'
-
-  get 'welcome/help'
-
-  get 'welcome/host'
-
   devise_for :users
+  
+  resources :home_listings
   
   root 'welcome#index'
 end
