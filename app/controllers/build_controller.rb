dd5eb8dfeb3c6f2646421dd4240fcf0cd5a6c726
Charlie Gaines <beaugaines@gmail.com>
2017-07-07 13:55:36 -0400

    wizard - wip

--- app/controllers/build_controller.rb
+++ app/controllers/build_controller.rb
@@ -1,10 +1,31 @@
 class BuildController < ApplicationController
   include Wicked::Wizard
-  
+
   steps :place_type, :bedrooms, :bed_details, :baths, :location, :amenities, :shared_spaces, :photos, :description, :title
-  
-  def create
-    @home_listing = HomeListing.create(lessor: current_user)
-    redirect_to wizard_path(steps.first)
+
+  def show
+    @home_listing = HomeListing.where(lessor: current_user).first_or_create
+    case step
+    when :place_type
+      @home_listing.build_address
+    end
+    render_wizard
+  end
+
+  def update
+    @home_listing = HomeListing.where(lessor: current_user).first_or_create
+    case step
+    when :place_type
+      @home_listing.assign_attributes(place_type_params)
+    when :bedrooms
+    when :bed_details
+    end
+    render_wizard @home_listing
+  end
+
+  private
+
+  def place_type_params
+    params.require(:home_listing).permit(:title, :capacity)
   end
 end
