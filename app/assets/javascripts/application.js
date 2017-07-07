d8c5d151e6e7ce654ee26d0c094d855eae6b7d12
Charlie Gaines <beaugaines@gmail.com>
2017-07-07 15:04:48 -0400

    feature working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,61 +17,63 @@
 //= require bootstrap-sprockets
 //= require bootstrap-datepicker
 
-
-
-
 function openModal() {
-    $('document').ready(function(){
-        document.getElementById('room-modal').style.display = "block";
-    });
+  $("document").ready(function() {
+    document.getElementById("room-modal").style.display = "block";
+  });
 }
 
 function closeModal() {
-    document.getElementById('room-modal').style.display = "none";
-};
+  document.getElementById("room-modal").style.display = "none";
+}
 
 var slideIndex = 1;
 showSlide(1);
 
 function plusSlides(n) {
-  showSlide(slideIndex += n);
+  showSlide((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlide(slideIndex = n);
+  showSlide((slideIndex = n));
 }
 
 function showSlide(n) {
-    var i;
-    console.log(document.getElementsByClassName("roomSlide"));
-    
-    $('document').ready(function(){
-        var slides = document.getElementsByClassName('roomSlide');
-        if (n > slides.length) {
-            slideIndex = 1;
-        }
-        if (n < 1) {
-            slideIndex = slides.length;
-        }
-        for(i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        slides[slideIndex - 1].style.display = "block";
-        console.log(slides[slideIndex - 1]);
-    });
+  var i;
+  console.log(document.getElementsByClassName("roomSlide"));
+
+  $("document").ready(function() {
+    var slides = document.getElementsByClassName("roomSlide");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+    console.log(slides[slideIndex - 1]);
+  });
 }
 
-$('document').ready(function(){
-   document.getElementById('focus').focus();
+$("document").ready(function() {
+  document.getElementById("focus").focus();
 });
 
-$(document).on('turbolinks:load', function(){
-   $('input[name="datepicker"]').daterangepicker({
-       autoUpdateInput: true,
-       singleDatePicker: true,
-       showDropdowns: true,
-       locale: {
-            format: 'YYYY/MM/DD'
-       }
-   });
+$(document).on("turbolinks:load", function() {
+  $('#daterange').daterangepicker({
+    autoUpdateInput: true,
+    autoApply: true,
+    showDropdowns: true
+    // locale: {
+    //   format: "YYYY/MM/DD"
+    // }
+  });
+
+  $('#daterange').on('apply.daterangepicker', function(ev, picker) {
+    $('#booking_request_checkin').val(picker.startDate.format('YYYY-MM-DD'));
+    $('#booking_request_checkout').val(picker.endDate.format('YYYY-MM-DD'));
+  })
 });
