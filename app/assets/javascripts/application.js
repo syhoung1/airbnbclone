794452d1adcb88d84e1983f8b3b0e80a92522190
Charlie Gaines <beaugaines@gmail.com>
2017-07-10 13:37:41 -0400

    js working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,76 +17,68 @@
 //= require bootstrap-sprockets
 //= require bootstrap-datepicker
 
-
 /* global $ */
 
 function openModal() {
-    $("document").ready(function(){
-        document.getElementById("room-modal").style.display = "block";
-    });
+  $("document").ready(function() {
+    document.getElementById("room-modal").style.display = "block";
+  });
 }
 
 function closeModal() {
-    document.getElementById("room-modal").style.display = "none";
+  document.getElementById("room-modal").style.display = "none";
 }
 
 var slideIndex = 1;
 showSlide(1);
 
 function plusSlides(n) {
-  showSlide(slideIndex += n);
+  showSlide((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlide(slideIndex = n);
+  showSlide((slideIndex = n));
 }
 
 function showSlide(n) {
-    var i;
-    console.log(document.getElementsByClassName("roomSlide"));
-    
-    $("document").ready(function(){
-        var slides = document.getElementsByClassName("roomSlide");
-        if (n > slides.length) {
-            slideIndex = 1;
-        }
-        if (n < 1) {
-            slideIndex = slides.length;
-        }
-        for(i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        slides[slideIndex - 1].style.display = "block";
-        console.log(slides[slideIndex - 1]);
-    });
+  var i;
+  console.log(document.getElementsByClassName("roomSlide"));
+
+  $("document").ready(function() {
+    var slides = document.getElementsByClassName("roomSlide");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+    console.log(slides[slideIndex - 1]);
+  });
 }
 
-$("document").ready(function(){
-   document.getElementById("focus").focus();
-});
-
 $(document).on("turbolinks:load", function() {
-    $(".accept").on("click", function(event) {
-        console.log("testing");
-    });
-});
+  document.getElementById("focus").focus();
 
-$(document).on('turbolinks:load', function() { 
+  $(".accept").on("click", function(event) {
+    console.log("testing");
+  });
+
   $(".edit-field").on("click", function(event) {
-    event.target.parentElement.lastElementChild.style.display ="block";
+    event.target.parentElement.lastElementChild.style.display = "block";
   });
-});
 
-$(document).on("turbolinks:load", function() {
-  
   $("#daterange").daterangepicker({
-      autoUpdateInput: true,
-      autoApply: true,
-      showDropdowns: true,
+    autoUpdateInput: true,
+    autoApply: true,
+    showDropdowns: true
   });
-  
-  $("#daterange").on("apply.daterangepicker", function(ev, picker){
-      $("#booking_request_checkin").val(picker.startDate.format("YYYY-MM-DD"));
-      $("#booking_request_checkout").val(picker.endDate.format("YYYY-MM-DD"));
+
+  $("#daterange").on("apply.daterangepicker", function(ev, picker) {
+    $("#booking_request_checkin").val(picker.startDate.format("YYYY-MM-DD"));
+    $("#booking_request_checkout").val(picker.endDate.format("YYYY-MM-DD"));
   });
 });
--- db/migrate/20170707213044_add_place_type_to_home_listings.rb
+++ db/migrate/20170707213044_add_place_type_to_home_listings.rb
@@ -5,7 +5,6 @@ class AddPlaceTypeToHomeListings < ActiveRecord::Migration
     add_column :home_listings, :place, :string
     add_column :home_listings, :property_type, :string
     add_column :home_listings, :accommodation_situation, :string
-    add_column :home_listings, :beds, :integer
     add_column :home_listings, :shared_spaces, :text, array:true, default: []
     add_column :home_listings, :summary, :text
     add_column :home_listings, :good_for, :string
